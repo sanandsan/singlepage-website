92d113e7db6c0e86445ff095a2e6e64d0235dae4
sanandsan <sancreations1@gmail.com>
2020-06-01 13:37:21 -0700

    this commit represent the latest code

---
+++ javascript/home.js
@@ -0,0 +1,16 @@
+
+document.body.style.backgroundImage = "url('bg-img1.jpg')";
+document.addEventListener('DOMContentLoaded',()=> {
+    
+       console.log( document.body.style.backgroundColor)
+
+});
+function achieve(){
+    const changableData= document.getElementById("achievements").innerHTML;
+    document.getElementById("changing").innerHTML=changableData
+}
+
+function signup(){
+    const changableData= document.getElementById("signup").innerHTML;
+    document.getElementById("changing").innerHTML=changableData
+}
